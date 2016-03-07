1132971b8286e794609f820af674cf0bb3090572
Ubuntu <ubuntu@ip-172-31-42-66.us-west-2.compute.internal>
2016-03-07 11:22:11 +0000

    show var on portfoliolist

--- app/scripts/controllers/portfoliolist.js
+++ app/scripts/controllers/portfoliolist.js
@@ -8,7 +8,7 @@
  * Controller of the theVarApp
  */
 angular.module('theVarApp')
-  .controller('PortfoliolistCtrl', function ($scope,Portfolios,ActivateNavBar) {
+  .controller('PortfoliolistCtrl', function ($scope, Portfolios, ActivateNavBar, Assets, varCalc) {
 
     ActivateNavBar.portfolios();
 
@@ -32,4 +32,24 @@ angular.module('theVarApp')
       a=a.assets;
       return Object.keys(a).length;
     };
+
+    $scope.portfolioVaR=function(percentile, portfolio) {
+      var a = portfolio.assets;
+      if(!a) {
+        return 0;
+      }
+      var al = Assets.list();
+      var o = a.map(function(x) {
+        if(al.hasOwnProperty(x.src)) {
+          if(al[x.src].hasOwnProperty(x.symbol)) {
+            return al[x.src][x.symbol];
+          }
+        }
+        return null;
+      }).filter(function(x) { return !!x; });
+
+      return varCalc.portfolioVaR(percentile,o);
+    };
+
+   
   });
